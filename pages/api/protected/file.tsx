a6ebf8d7c5a69c62e0facacc99ec995fc339ee9d
Derik <derikbosing@gmail.com>
2024-07-31 04:42:42 -0300

    chore():added partners items

--- pages/api/protected/file.tsx
+++ pages/api/protected/file.tsx
@@ -62,49 +62,52 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             return res.status(500).json({ error: "Error parsing file" });
         }
 
-        const uploadedFiles = Array.isArray(files.file) ? files.file : [files.file];
+        try {
 
-        if (uploadedFiles.length === 0) {
-            return res.status(400).json({ error: "No files found" });
+            const uploadedFiles = Array.isArray(files.file) ? files.file : [files.file];
+
+            if (uploadedFiles.length === 0) {
+                return res.status(400).json({ error: "No files found" });
+            }
+
+            const imageNames = Array.isArray(fields.imageName) ? fields.imageName : [fields.imageName];
+            const imageDescriptions = Array.isArray(fields.imageDescription) ? fields.imageDescription : [fields.imageDescription];
+
+            const currentYear = new Date().getFullYear();
+            const uploadsDir = path.join(process.cwd(), 'public', process.env.UPLOADS_DIRECTORY!, currentYear.toString());
+
+            if (!fs.existsSync(uploadsDir)) {
+                fs.mkdirSync(uploadsDir, { recursive: true });
+            }
+
+            const imageRecords = [];
+            for (let index = 0; index < uploadedFiles.length; index++) {
+                const file = uploadedFiles[index];
+                const lastImage = await prisma.image.findFirst({
+                    orderBy: { id: 'desc' }
+                });
+                const imageId = lastImage ? lastImage.id + 1 : 1;
+                const fileName = `${imageId}-${file!.originalFilename}`;
+                const filePath = path.join(uploadsDir, fileName);
+
+                fs.copyFileSync(file!.filepath, filePath);
+                const urlPath = `${process.env.UPLOADS_DIRECTORY}/${currentYear}/${fileName}`;
+                const image = await prisma.image.create({
+                    data: {
+                        url: urlPath,
+                        name: imageNames[index],
+                        description: imageDescriptions[index],
+                        projectId: fields.projectId ? fields.projectId[0] : null,
+                        userId: (session as any).user.id,
+                    },
+                });
+
+                imageRecords.push(image);
+            }
+
+            res.status(200).json({ message: 'Upload successful', files: imageRecords });
+        } catch (error) {
+            res.status(500).json({ error: "Error processing file" });
         }
-
-        const imageNames = Array.isArray(fields.imageName) ? fields.imageName : [fields.imageName];
-        const imageDescriptions = Array.isArray(fields.imageDescription) ? fields.imageDescription : [fields.imageDescription];
-
-        const currentYear = new Date().getFullYear();
-        const uploadsDir = path.join(process.cwd(), 'public', process.env.UPLOADS_DIRECTORY!, currentYear.toString());
-
-        if (!fs.existsSync(uploadsDir)) {
-            fs.mkdirSync(uploadsDir, { recursive: true });
-        }
-
-        const imageRecords = [];
-
-        for (let index = 0; index < uploadedFiles.length; index++) {
-            const file = uploadedFiles[index];
-            const lastImage = await prisma.image.findFirst({
-                orderBy: { id: 'desc' }
-            });
-            const imageId = lastImage ? lastImage.id + 1 : 1;
-            const fileName = `${imageId}-${file!.originalFilename}`;
-            const filePath = path.join(uploadsDir, fileName);
-
-            fs.copyFileSync(file!.filepath, filePath);
-
-            const urlPath = `${process.env.UPLOADS_DIRECTORY}/${currentYear}/${fileName}`;
-            const image = await prisma.image.create({
-                data: {
-                    url: urlPath,
-                    name: imageNames[index],
-                    description: imageDescriptions[index],
-                    projectId: fields.projectId ? fields.projectId[0] : null,
-                    userId: (session as any).user.id,
-                },
-            });
-
-            imageRecords.push(image);
-        }
-
-        res.status(200).json({ message: 'Upload successful', files: imageRecords });
     });
 }
