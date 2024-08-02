4723eb1a46ecf4bd90675c619a700362c806b807
Derik <derikbosing@gmail.com>
2024-08-02 00:29:56 -0300

    chore:auth adjustment

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,5 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/admin/:path*", "/api/protected/:path*"] };
+export const config = {
+  matcher: ["/admin/:path*", "/api/protected/:path*", "/admin"],
+};
