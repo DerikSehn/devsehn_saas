c7c7bdc9689c3008bab39a715628912c5c3bcb4a
Derik <derikbosing@gmail.com>
2024-09-12 18:44:52 -0300

    chore: typescript error fixed

--- types/image-type.ts
+++ types/image-type.ts
@@ -1,14 +1,21 @@
-import { Image } from "@prisma/client";
+import { Image } from '@prisma/client';
 
 export interface ImageType
   extends Omit<
     Image,
-    "createdAt" | "updatedAt" | "projectId" | "userId" | "sectionId" | "id"
+    | 'createdAt'
+    | 'updatedAt'
+    | 'projectId'
+    | 'userId'
+    | 'sectionId'
+    | 'id'
+    | 'postId'
   > {
   id?: number;
   createdAt?: Date;
   updatedAt?: Date;
   projectId?: number | null;
+  postId?: number | null;
   userId?: string | null;
   sectionId?: number | null;
 }
