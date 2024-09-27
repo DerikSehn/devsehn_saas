7b9d18b8667506ad1e309d4487ba09b4c3756bc6
Derik Sehn <derikbosing@gmail.com>
2024-09-27 12:14:04 +0000

    chore(seo): changed script ldjson at document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,22 +12,21 @@ export default function Document() {
         <meta property="og:description" content="Cultura Verde oferece os melhores serviços de paisagismo." />
         <meta property="og:image" content="https://culturaverde.com.br/logo.png" />
         <meta property="og:url" content="https://culturaverde.com.br" />
-        <script type="application/ld+json">
-          {`
-            {
-              "@context": "https://schema.org",
-              "@type": "Organization",
-              "name": "Cultura Verde - a excelência em paisagismo",
-              "url": "https://culturaverde.com.br",
-              "logo": "https://culturaverde.com.br/logo.png",
-              "sameAs": [
-                "https://www.facebook.com/culturaverde",
-                "https://www.instagram.com/cultura_verde",
-                "https://www.facebook.com/culturaverders"
-              ]
-            }
-          `}
-        </script>
+        <script type="application/ld+json" dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "Cultura Verde - a excelência em paisagismo",
+            "url": "https://culturaverde.com.br",
+            "logo": "https://culturaverde.com.br/logo.png",
+            "sameAs": [
+              "https://www.facebook.com/culturaverde",
+              "https://www.instagram.com/cultura_verde",
+              "https://www.facebook.com/culturaverders"
+            ]
+          })
+        }} />
+
       </Head>
       <body className="selection:bg-primary-200 selection:text-primary-900 selection:text-opacity-100 selection:bg-opacity-50">
         <Main />
