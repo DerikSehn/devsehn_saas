ec48beb7905f5959c4a5ce01f9f0aa61b99dd313
Derik <derikbosing@gmail.com>
2024-10-09 23:40:08 -0300

    chore(_document): add google tag manager scripts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,9 @@
+
+
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
-
-
   return (
     <Html lang="en">
       <Head>
@@ -26,9 +27,23 @@ export default function Document() {
             ]
           })
         }} />
-
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager-script" dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NZDTDC5B');`
+        }} />
+        {/* End Google Tag Manager */}
       </Head>
       <body className="selection:bg-primary-200 selection:text-primary-900 selection:text-opacity-100 selection:bg-opacity-50">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZDTDC5B"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
